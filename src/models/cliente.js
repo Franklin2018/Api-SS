e782d7639ab05fe1e5da8c07b74a13093801c67b
Franklin <franklin.ojc@gmail.com>
2020-10-06 15:14:58 -0400

    se configuro la conexion para la db alojada en heroku

--- src/models/cliente.js
+++ src/models/cliente.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 
 connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'db_sistema_seguros'
+    host: 'us-cdbr-east-02.cleardb.com/',
+    user: 'b1a53e8c72d40d',
+    password: '6576bf4b',
+    database: 'heroku_2a2cafbb71575e6'
 });
-
+/*mysql://b1a53e8c72d40d:6576bf4b@us-cdbr-east-02.cleardb.com/heroku_2a2cafbb71575e6?reconnect=true*/
 let clienteModel = {};
 
 clienteModel.getClientes = (callback)=> {
